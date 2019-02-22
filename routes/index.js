fab7020359b13016ec8489dbd73cdd0acd555fcf
Space School <space.school.kg@gmail.com>
2019-02-22 20:23:25 +0600

    check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+let dirname = __dirname.replace('\\routes', '')
+dirname = dirname.replace('/routes', '')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log('client')
+    res.sendFile(path.join(dirname, 'aclient', 'index.html'));
+
 });
 
 module.exports = router;
