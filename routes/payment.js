d55438fa547c809384dda4ec432a6499dac805f1
Space School <space.school.kg@gmail.com>
2019-02-27 10:45:08 +0600

    check

--- routes/payment.js
+++ routes/payment.js
@@ -7,11 +7,11 @@ var xml = require('xml');
 /* GET home page. */
 router.get('/asisnur', async (req, res, next) => {
     try{
-        console.log(req.body)
+        console.log(req.param('command'))
         let result;
         res.set('Content-Type', 'text/xml');
-        if(req.body.command==='check'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.body.account})
+        if(req.param.command==='check'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
             /*XML*/
             if(wallet!=null){
                 result = [ { response: [ { result: 0 } , { comment: 'ok' } ] } ];
@@ -22,18 +22,18 @@ router.get('/asisnur', async (req, res, next) => {
                 res.status(200);
                 res.end(xml(result, true));
             }
-        } else if(req.body.command==='pay'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.body.account})
+        } else if(req.param('command')==='pay'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
             if(wallet!=null){
-                wallet.balance = parseInt(wallet.balance)-parseInt(req.body.sum)
+                wallet.balance = parseInt(wallet.balance)-parseInt(req.param('sum'))
                 await WalletBiletiki.findOneAndUpdate({_id: wallet._id}, {$set: wallet});
-                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.body.sum), service: 'AsisNur', meta:'Дата: '+req.body.txn_date+' \nID: '+req.body.txn_id});
+                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.param('sum')), service: 'AsisNur', meta:'Дата: '+req.param('txn_date')+' \nID: '+req.param('txn_id')});
                 await PaymentBiletiki.create(payment);
-                result = [ { response: [ { txn_id: req.body.txn_id } , { result: 0 } , { comment: 'no such user' } ] } ];
+                result = [ { response: [ { txn_id: req.param('txn_id') } , { result: 0 } , { comment: 'no such user' } ] } ];
                 res.status(200);
                 res.end(result);
             } else {
-                result = [ { response: [ { txn_id: req.body.txn_id } , { result: 1 } , { comment: 'no such user' } ] } ];
+                result = [ { response: [ { txn_id: req.param('txn_id') } , { result: 1 } , { comment: 'no such user' } ] } ];
                 res.status(200);
                 res.end(result);
             }
@@ -46,33 +46,34 @@ router.get('/asisnur', async (req, res, next) => {
 
 router.get('/qiwi', async (req, res, next) => {
     try{
-        console.log(req.body)
+        console.log(req.param('command'))
         let result;
         res.set('Content-Type', 'text/xml');
-        if(req.body.command==='check'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.body.account})
+        if(req.param('command')==='check'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
             /*XML*/
             if(wallet!=null){
-                result = [ { response: [ { osmp_txn_id: req.body.txn_id } , { result: 0 }, {comment: 'ok'} ] } ];
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 0 }, {comment: 'ok'} ] } ];
                 res.status(200);
                 res.end(xml(result, true));
             } else {
-                result = [ { response: [ { osmp_txn_id: req.body.txn_id } , { result: 1 }, {comment: 'ok'} ] } ];
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 1 }, {comment: 'ok'} ] } ];
                 res.status(200);
                 res.end(xml(result, true));
             }
-        } else if(req.body.command==='pay'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.body.account})
+        } else if(req.param('command')==='pay'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
+            console.log(wallet)
             if(wallet!=null){
-                wallet.balance = wallet.balance-parseInt(req.body.sum)
+                wallet.balance = wallet.balance-parseInt(req.param('sum'))
                 await WalletBiletiki.findOneAndUpdate({_id: wallet._id}, {$set: wallet});
-                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.body.sum), service: 'QIWI', meta:'Дата: '+req.body.txn_date+' \nID: '+req.body.txn_id});
+                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.param('sum')), service: 'QIWI', meta:'Дата: '+req.param('txn_date')+' \nID: '+req.param('txn_id')});
                 await PaymentBiletiki.create(payment);
-                result = [ { response: [ { osmp_txn_id: req.body.txn_id } , { prv_txn: payment._id } , { sum: req.body.sum } , { result: 0 } , { comment: 'ok' } ] } ];
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: payment._id } , { sum: req.param('sum') } , { result: 0 } , { comment: 'ok' } ] } ];
                 res.status(200);
                 res.end(xml(result, true));
             } else {
-                result = [ { response: [ { osmp_txn_id: req.body.txn_id } , { prv_txn: '' } , { sum: req.body.sum } , { result: 1 } , { comment: 'no such user' } ] } ];
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: '' } , { sum: req.param('sum') } , { result: 1 } , { comment: 'no such user' } ] } ];
                 res.status(200);
                 res.end(xml(result, true));
             }
@@ -82,5 +83,45 @@ router.get('/qiwi', async (req, res, next) => {
         res.status(501);
     }
 });
+
+router.get('/balancekg', async (req, res, next) => {
+    try{
+        console.log(req.param('command'))
+        let result;
+        res.set('Content-Type', 'text/xml');
+        if(req.param('command')==='check'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
+            /*XML*/
+            if(wallet!=null){
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 0 }, {comment: 'ok'} ] } ];
+                res.status(200);
+                res.end(xml(result, true));
+            } else {
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 1 }, {comment: 'ok'} ] } ];
+                res.status(200);
+                res.end(xml(result, true));
+            }
+        } else if(req.param('command')==='pay'){
+            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
+            if(wallet!=null){
+                wallet.balance = wallet.balance-parseInt(req.param('sum'))
+                await WalletBiletiki.findOneAndUpdate({_id: wallet._id}, {$set: wallet});
+                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.param('sum')), service: 'balancekg', meta:'Дата: '+req.param('txn_date')+' \nID: '+req.param('txn_id')});
+                await PaymentBiletiki.create(payment);
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: payment._id } , { sum: req.param('sum') } , { result: 0 } , { comment: 'ok' } ] } ];
+                res.status(200);
+                res.end(xml(result, true));
+            } else {
+                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: '' } , { sum: req.param('sum') } , { result: 1 } , { comment: 'no such user' } ] } ];
+                res.status(200);
+                res.end(xml(result, true));
+            }
+        }
+    } catch(error) {
+        console.error(error)
+        res.status(501);
+    }
+});
+
 
 module.exports = router;
