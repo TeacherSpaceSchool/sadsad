b3edc668648eee71941f771d6edbf36128ff8bf3
Space School <space.school.kg@gmail.com>
2019-02-28 14:24:29 +0600

    check

--- routes/payment.js
+++ routes/payment.js
@@ -102,35 +102,40 @@ router.get('/balancekg', async (req, res, next) => {
     try{
         let ip = JSON.stringify(req.ip)
         console.log(ip.includes('212.112.122.179'))
-        let result;
-        res.set('Content+Type', 'text/xml');
-        if(req.param('command')==='check'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
-            /*XML*/
-            if(wallet!=null){
-                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 0 }, {comment: 'ok'} ] } ];
-                res.status(200);
-                res.end(xml(result, true));
-            } else {
-                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 1 }, {comment: 'ok'} ] } ];
-                res.status(200);
-                res.end(xml(result, true));
+        if(ip.includes('212.112.122.179')){
+            let result;
+            res.set('Content+Type', 'text/xml');
+            if(req.param('command')==='check'){
+                let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
+                /*XML*/
+                if(wallet!=null){
+                    result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 0 }, {comment: 'ok'} ] } ];
+                    res.status(200);
+                    res.end(xml(result, true));
+                } else {
+                    result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { result: 1 }, {comment: 'ok'} ] } ];
+                    res.status(200);
+                    res.end(xml(result, true));
+                }
+            } else if(req.param('command')==='pay'){
+                let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
+                if(wallet!=null){
+                    wallet.balance = wallet.balance+parseInt(req.param('sum'))
+                    await WalletBiletiki.findOneAndUpdate({_id: wallet._id}, {$set: wallet});
+                    let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.param('sum')), service: 'balancekg', meta:'Дата: '+new Date(parseInt(req.param('txn_date')))+' \nID: '+req.param('txn_id')});
+                    await PaymentBiletiki.create(payment);
+                    result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: payment._id } , { sum: req.param('sum') } , { result: 0 } , { comment: 'ok' } ] } ];
+                    res.status(200);
+                    res.end(xml(result, true));
+                } else {
+                    result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: '' } , { sum: req.param('sum') } , { result: 1 } , { comment: 'no such user' } ] } ];
+                    res.status(200);
+                    res.end(xml(result, true));
+                }
             }
-        } else if(req.param('command')==='pay'){
-            let wallet = await WalletBiletiki.findOne({wallet: req.param('account')})
-            if(wallet!=null){
-                wallet.balance = wallet.balance+parseInt(req.param('sum'))
-                await WalletBiletiki.findOneAndUpdate({_id: wallet._id}, {$set: wallet});
-                let payment = new PaymentBiletiki({user: wallet.user, ammount: parseInt(req.param('sum')), service: 'balancekg', meta:'Дата: '+new Date(parseInt(req.param('txn_date')))+' \nID: '+req.param('txn_id')});
-                await PaymentBiletiki.create(payment);
-                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: payment._id } , { sum: req.param('sum') } , { result: 0 } , { comment: 'ok' } ] } ];
-                res.status(200);
-                res.end(xml(result, true));
-            } else {
-                result = [ { response: [ { osmp_txn_id: req.param('txn_id') } , { prv_txn: '' } , { sum: req.param('sum') } , { result: 1 } , { comment: 'no such user' } ] } ];
-                res.status(200);
-                res.end(xml(result, true));
-            }
+        } else {
+            console.error(req.ip)
+            res.status(501);
         }
     } catch(error) {
         console.error(error)
