40a401b02e4f4261844b6c31445839ded22f7234
Space School <space.school.kg@gmail.com>
2019-03-14 19:16:11 +0600

    check

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(logger('dev'));
 //app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
-app.use(bodyParser.text());
-app.use(bodyParser.json());
+app.use(bodyParser.text({type:'text/*'}));
+//app.use(bodyParser.json());
 app.use(bodyParser.xml());
 app.use(express.static(path.join(__dirname, 'aclient')));
 app.use(express.static(path.join(__dirname, 'public')));
