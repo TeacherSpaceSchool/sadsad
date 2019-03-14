b76b26ffbf10a2b0b62e4b8806e245d76f984252
Space School <space.school.kg@gmail.com>
2019-03-14 19:03:37 +0600

    check

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ connectDB.connect()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(bodyParser.json());
 //app.use(bodyParser.text());
