5c73fa769921f381c852896275014f9cc6f9a22f
Space School <space.school.kg@gmail.com>
2019-03-14 14:29:37 +0600

    check

--- routes/payment.js
+++ routes/payment.js
@@ -432,8 +432,7 @@ router.post('/elsom/pay', async (req, res, next) => {
 router.post('/kcb/check', async (req, res, next) => {
     res.set('Content+Type', 'text/xml');
     try{
-        console.log(req.body, req.params)
-        console.log(req)
+        console.log(req.body.elements[0].elements)
         let ip = JSON.stringify(req.ip)
         let result = {}
         if(ip.includes('95.46.154.64')){
