db716dc3f75fd224147f33f405a8535af9e653d6
Space School <space.school.kg@gmail.com>
2019-03-16 10:43:10 +0600

    check

--- module/ticketBiletiki.js
+++ module/ticketBiletiki.js
@@ -83,7 +83,7 @@ const buy = async (req, res, user) => {
             doc.moveDown()
             //doc.addPage()
             //doc.moveDown()
-            doc.image(qrpath, {width: 145, align: 'center'})
+            doc.image(qrpath, {fit: [145, 145], align: 'center'})
             doc
                 .font('NotoSans')
                 .fontSize(11)
