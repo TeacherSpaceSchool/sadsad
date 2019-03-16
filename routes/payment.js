0fa4fc64068cb7daa7697aecedcb91520be430ff
Space School <space.school.kg@gmail.com>
2019-03-16 18:41:08 +0600

    check

--- routes/payment.js
+++ routes/payment.js
@@ -582,11 +582,14 @@ router.post('/kcb', async (req, res, next) => {
 
 router.post('/balance/generate', async (req, res, next) => {
     try{
-        res.set('Content+Type', 'text/json; charset=utf-8');
-        let data = new FormData();
+        /*let data = new FormData();
         data.append('merchant', 'KASSIR_KG');
         data.append('password', 'Q3Rup+pE');
-        let auth_token = await axios.post('https://umai.balance.kg/site-api/acquiring/auth', data)
+        let auth_token = await axios.post('https://umai.balance.kg/site-api/acquiring/auth', data)*/
+        let auth_token = await axios.post('https://umai.balance.kg/site-api/acquiring/auth', {
+            merchant: 'KASSIR_KG',
+            password: 'Q3Rup+pE'
+        })
         console.log(auth_token)
         /*if(auth_token.data.status=='FAIL'){
             res.status(200);
