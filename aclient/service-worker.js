8b1f5d4fecd281c286bf6455e0ff5730113a39f7
Space School <space.school.kg@gmail.com>
2019-09-07 19:00:37 +0600

    check

--- aclient/service-worker.js
+++ aclient/service-worker.js
@@ -1,6 +1,13 @@
 importScripts("/precache-manifest.22e4a03abd46f1ee622726cdae546d0c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-workbox.precaching.precacheAndRoute (self .__ precacheManifest || [])
+console.log('start-sw')
+self.addEventListener('install', event => {
+    console.log('install')
+})
+self.addEventListener('activate', event => {
+    console.log('activate')
+})
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 workbox.routing.registerRoute(
     new RegExp('https:.*(jpg|jpeg)'),
     workbox.strategies.staleWhileRevalidate({
